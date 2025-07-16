8854567fa17f8c215a51548e9c6f0f7532017afb
Avina Jain <avinajain03@gmail.com>
2025-07-17 01:34:17 +0530

    chore: upgrade angular version to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { InputGroupModule } from 'primeng/inputgroup';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    // Add the missing imports for PrimeNG modules
-    InputTextModule,
-    ButtonModule,
-    CardModule,
-    ProgressSpinnerModule,
-    InputGroupModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SearchComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        // Add the missing imports for PrimeNG modules
+        InputTextModule,
+        ButtonModule,
+        CardModule,
+        ProgressSpinnerModule,
+        InputGroupModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
