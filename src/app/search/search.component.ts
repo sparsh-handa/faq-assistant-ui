6ed5fb1d6c749211f22115047a40b0a3de9a1738
Avina Jain <avinajain03@gmail.com>
2025-07-17 00:39:47 +0530

    chore:000 - initial commit

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent {
+  query: string = '';
+  results: any[] = [];
+  loading: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  searchFAQ() {
+    if (!this.query.trim()) return;
+    this.loading = true;
+    this.http.post<any[]>('http://127.0.0.1:8000/api/search', { text: this.query })
+      .subscribe({
+        next: (res) => {
+          this.results = res;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error(err);
+          this.loading = false;
+        }
+      });
+  }
+  resetSearch()
+  {
+    this.query = '';
+    this.results = [];
+    this.loading = false;
+  }
+}
